8f984e0d8880cf0bc5eab48f7da8751fd33fabe3
Moises0011 <moisesnascfernandes@gmail.com>
2022-06-16 09:51:27 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+n1 = int (input("Digite um numero: "))
+n2 = int (input("Digite outro numero: "))
+
+print ("=====================================")
+
+soma = n1 + n2
+print ("Resultado da soma: ",soma)
+
+print ("=====================================")
+
+sub = n1 - n2 
+print ("Resultado da subtração: ",sub)
+
+print ("=====================================")
+
+div = n1 / n2 
+print ("Resultado da divisão: ",div)
+
+print ("=====================================")
+
+mult = n1 * n2 
+print ("Resultado da multiplicação: ",mult)
